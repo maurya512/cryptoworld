29b468987f9778d9031058373d9adc2c62f012fd
maurya512 <patelmaurya0512@gmail.com>
2022-02-09 15:29:54 -0500

    displayed top 6 crypto news with working links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,21 +24,21 @@ const Navbar = () => {
         {/* <Button className='menu-control-container'>
 
           </Button> */}
-        <Menu theme="dark">
-          <Menu.Item icon={<HomeOutlined />}>
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item icon={<FundOutlined />}>
-            <Link to="/cryptocurrencies">Crypto Currencies</Link>
-          </Menu.Item>
-          <Menu.Item icon={<MoneyCollectOutlined />}>
-            <Link to="/exchanges">Exchanges</Link>
-          </Menu.Item>
-          <Menu.Item icon={<BulbOutlined />}>
-            <Link to="/news">News</Link>
-          </Menu.Item>
-        </Menu>
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Crypto Currencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,20 +3,52 @@ import React from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
+// import { Text, Title } from Typography
 // ! importing the query to search and display news from the api
 import { useGetCryptoNewsQuery } from "../../services/newsApi";
 
-import { Text, Title } from "antd";
+import demoImage from "../../images/cryptocurrency.png";
+
 // import { Option } from Select;
 
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrency",
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 12,
   });
 
-  console.log(cryptoNews);
-  return <div></div>;
+  // * looping over the news
+  if (!cryptoNews?.value) return "Loading....";
+  return (
+    <div>
+      <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((news, i) => (
+          <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Typography.Title className="news-title" level={4}>
+                    {news.name}
+                  </Typography.Title>
+                  {/* * image for news */}
+                  {/* * display the news' image or a dummy image from folder */}
+                  <img
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
+                    alt="news"
+                  />
+                </div>
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description}
+                </p>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default News;
