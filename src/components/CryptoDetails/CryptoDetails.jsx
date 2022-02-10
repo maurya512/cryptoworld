5d4ac462617caa1b84c523b032b3fc675f7985fd
maurya512 <patelmaurya0512@gmail.com>
2022-02-10 16:25:05 -0500

    api changes after api udpate

--- src/components/CryptoDetails/CryptoDetails.jsx
+++ src/components/CryptoDetails/CryptoDetails.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+// ! import dependencies
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+
+import { useGetCryptoDetailsQuery } from "../../services/cryptoApi";
+// * import text, title and option from typography and select
 
 const CryptoDetails = () => {
-  return <div></div>;
+  // * displays the currency based on the coin id using useParams
+  const { coinId } = useParams();
+
+  // * time period use state
+  const [timePeriod, setTimePeriod] = useState("7d");
+
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+
+  console.log(data);
+  return <div>CryptoDetails {coinId}</div>;
 };
 
 export default CryptoDetails;
