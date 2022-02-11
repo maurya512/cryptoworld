9ab75206f0d26a6220a43d8f4fbbf3b653b4b511
maurya512 <patelmaurya0512@gmail.com>
2022-02-10 22:40:36 -0500

    final changes

--- src/App.js
+++ src/App.js
@@ -38,11 +38,11 @@ const App = () => {
                 <Space >
                     <Link to="/">Home</Link>
                     <Link to="/exchanges">Exchanges</Link>
-                    <Link to="/news">News</Link>
+                <Link to="/news">News</Link>
                 </Space>
             </div>
         </div>
-    </div>;
+    </div>
 };
 
 export default App;
